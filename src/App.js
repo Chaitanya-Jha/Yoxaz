372cb76c0f644a135e709546f7c8d1d8c7221621
Chaitanya <chaitanyajha@Chaitanyas-MacBook-Air.local>
2024-02-19 18:51:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/Header";
+import Table from "./component/Table";
+import CreateNew from "./component/CreateNew";
+import "./App.css";
+import { useState } from "react";
+function App() {
+  const [rowss, setRows] = useState([]);
+  const addRow = (formData) => {
+    setRows([...rowss, formData]);
+  };
+  return (
+    <div className="app">
+      <div className="container1">
+        <Header />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Table rows={rowss} />} />
+            <Route path="/create-new" element={<CreateNew addRow={addRow} />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  );
+}
+export default App;
